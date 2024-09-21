f2affe20f258500cccfddb12cdf822e5fe1c1dbb
debasis <debasispanda3454@gmail.com>
2024-09-21 21:33:15 +0530

    feat: create-of-projects-workspace

--- tfe-resources/main.tf
+++ tfe-resources/main.tf
@@ -1,7 +1,68 @@
-# # Create an organization
-# resource "tfe_organization" "organization-01" {
-#   name  = var.tfe_organization
-#   email = var.TFE_MAIL_ID
-# }
+// tfe auth creation
+resource "tfe_oauth_client" "auth-01" {
+  organization     = var.tfe_organization
+  api_url          = "https://api.github.com"
+  http_url         = "https://github.com"
+  oauth_token      = var.TFE_GH_TOKEN
+  service_provider = "github"
+}
+######################
+# tfe Projects lists #
+######################
+resource "tfe_project" "ps-01" {
+  organization = var.tfe_organization
+  name         = "azure-authentication"
+}
 
-//
+resource "tfe_project" "ps-02" {
+  organization = var.tfe_organization
+  name         = "azure-cloud-setup-rsg"
+}
+
+resource "tfe_project" "ps-03" {
+  organization = var.tfe_organization
+  name         = "azure-cloud-setup-sql"
+}
+
+#######################
+# tfe Workspace lists #
+#######################
+
+resource "tfe_workspace" "ws-01" {
+  name              = "azure-authentication-ws-01"
+  organization      = var.tfe_organization
+  queue_all_runs    = false
+  project_id        = tfe_project.ps-01.id
+  working_directory = "/azure-resources"
+  vcs_repo {
+    branch         = var.tfe_branch
+    identifier     = "debasis112/azure-autentication"
+    oauth_token_id = tfe_oauth_client.auth-01.oauth_token_id
+  }
+}
+
+resource "tfe_workspace" "ws-02" {
+  name              = "azure-cloud-setup-rsg-ws-02"
+  organization      = var.tfe_organization
+  queue_all_runs    = false
+  project_id        = tfe_project.ps-02.id
+  working_directory = "/resource-group"
+  vcs_repo {
+    branch         = var.tfe_branch
+    identifier     = "debasis112/azure-cloud-setup"
+    oauth_token_id = tfe_oauth_client.auth-01.oauth_token_id
+  }
+}
+
+resource "tfe_workspace" "ws-03" {
+  name              = "azure-cloud-setup-sql-ws-03"
+  organization      = var.tfe_organization
+  queue_all_runs    = false
+  project_id        = tfe_project.ps-03.id
+  working_directory = "/ms-sql"
+  vcs_repo {
+    branch         = var.tfe_branch
+    identifier     = "debasis112/azure-cloud-setup"
+    oauth_token_id = tfe_oauth_client.auth-01.oauth_token_id
+  }
+}
--- tfe-resources/variables.tf
+++ tfe-resources/variables.tf
@@ -4,14 +4,21 @@ variable "tfe_organization" {
   default     = "debasis-project-dissertation"
 }
 
+variable "tfe_branch" {
+  type        = string
+  description = "The branch for GitHub"
+  default     = "main"
+  sensitive   = false
+}
+
 variable "TFE_API_TOKEN" {
   type        = string
   description = "The API token for Terraform Cloud"
   sensitive   = true
 }
 
-# variable "tfe_gh_token" {
-#   type        = string
-#   description = "The PAT token for GitHub"
-#   sensitive   = true
-# }
+variable "TFE_GH_TOKEN" {
+  type        = string
+  description = "The PAT token for GitHub"
+  sensitive   = true
+}
